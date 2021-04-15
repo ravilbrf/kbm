502e98ae6045a86d467af24d8fff78f495079585
Ravil <brf@li.ru>
2021-04-15 13:01:31 +0400

    init4

--- config/app.php
+++ config/app.php
@@ -175,11 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-
-        // My Provider
-        Barryvdh\Debugbar\ServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-
     ],
 
     /*
